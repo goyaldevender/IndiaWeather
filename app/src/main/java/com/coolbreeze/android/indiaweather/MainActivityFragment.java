5c9efe2f6c0593436188c742bd3d3831b9b37ebf
Devender Goyal <goyaldevender@users.noreply.github.com>
2016-01-26 19:33:36 +0530

    Change of Icon from pixabay

--- app/src/main/java/com/coolbreeze/android/indiaweather/MainActivityFragment.java
+++ app/src/main/java/com/coolbreeze/android/indiaweather/MainActivityFragment.java
@@ -1,10 +1,23 @@
 package com.coolbreeze.android.indiaweather;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -16,7 +29,87 @@ public class MainActivityFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        String[] forecastArray;
+        forecastArray = new String[]{
+                "Today - Sunny - 12/32",
+                "Tomorrow - Sunny - 12/32",
+                "Weds - Sunny - 12/32",
+                "Thurs - Sunny - 12/32",
+                "Fri - Sunny - 12/32",
+                "Sat - Sunny - 12/32",
+                "Sun - Sunny - 12/32",
+        };
+
+//        // These two need to be declared outside the try/catch
+//        // so that they can be closed in the finally block.
+//        HttpURLConnection urlConnection = null;
+//        BufferedReader reader = null;
+//
+//        // Will contain the raw JSON response as a string.
+//        String forecastJsonStr = null;
+//
+//        try {
+//            // Construct the URL for the OpenWeatherMap query
+//            // Possible parameters are avaiable at OWM's forecast API page, at
+//            // http://openweathermap.org/API#forecast
+//            URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7");
+//
+//            // Create the request to OpenWeatherMap, and open the connection
+//            urlConnection = (HttpURLConnection) url.openConnection();
+//            urlConnection.setRequestMethod("GET");
+//            urlConnection.connect();
+//
+//            // Read the input stream into a String
+//            InputStream inputStream = urlConnection.getInputStream();
+//            StringBuffer buffer = new StringBuffer();
+//            if (inputStream == null) {
+//                // Nothing to do.
+//                return null;
+//            }
+//            reader = new BufferedReader(new InputStreamReader(inputStream));
+//
+//            String line;
+//            while ((line = reader.readLine()) != null) {
+//                // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+//                // But it does make debugging a *lot* easier if you print out the completed
+//                // buffer for debugging.
+//                buffer.append(line + "\n");
+//            }
+//
+//            if (buffer.length() == 0) {
+//                // Stream was empty.  No point in parsing.
+//                return null;
+//            }
+//            forecastJsonStr = buffer.toString();
+//        } catch (IOException e) {
+//            Log.e("PlaceholderFragment", "Error ", e);
+//            // If the code didn't successfully get the weather data, there's no point in attemping
+//            // to parse it.
+//            return null;
+//        } finally {
+//            if (urlConnection != null) {
+//                urlConnection.disconnect();
+//            }
+//            if (reader != null) {
+//                try {
+//                    reader.close();
+//                } catch (final IOException e) {
+//                    Log.e("PlaceholderFragment", "Error closing stream", e);
+//                }
+//            }
+//        }
+
+        List<String> weekForcast = new ArrayList<String>(Arrays.asList(forecastArray));
+
+        ArrayAdapter<String> mForecastAdaptor = new ArrayAdapter<String>(getActivity(),
+                R.layout.list_item_forecast, R.id.list_item_forecast_textview, weekForcast);
+
+        ListView listview = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listview.setAdapter(mForecastAdaptor);
+
+        return rootView;
     }
 }
